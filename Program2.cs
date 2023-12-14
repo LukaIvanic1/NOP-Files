7312e8c114dc70c6de3773767b28b676df1af452
Luka Ivanić <107309593+LukaIvanic1@users.noreply.github.com>
2023-12-14 12:03:49 +0100

    Add files via upload

---
+++ Program2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList jao = new ArrayList();
+            jao.Add(1);
+            jao.Add("string");
+            jao.Add('a');
+            jao.Add(10.31);
+
+            
+
+            foreach (var item in jao)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Console.ReadKey();
+        
+        }
+    }
+}
