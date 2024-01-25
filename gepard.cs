d64c3e66ef5114985dcea199db8a19c82a36b794
Luka Ivanić <107309593+LukaIvanic1@users.noreply.github.com>
2024-01-25 10:17:56 +0100

    Add files via upload

---
+++ gepard.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nasljedjivanje1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Gepard g = new Gepard();
+           // Console.WriteLine(g.Opasna);
+            g.Opasnost();
+           // Console.WriteLine(g.Opasna);
+            Console.ReadKey();
+
+        }
+    }
+    class Cat
+    {
+        public bool Opasna { get; set; }
+        void Opasnost()
+        {
+            Opasna = false;
+        }
+        
+    }
+
+    class Gepard : Cat
+    {
+        public bool Opasna { get; set; }
+        public void Opasnost()
+        {
+            Opasna = true;
+        }
+    }
+
+
+}
