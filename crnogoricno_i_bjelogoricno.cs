8200f691d98e68cfb5bc8b5f3160157e146479bd
Luka Ivanić <107309593+LukaIvanic1@users.noreply.github.com>
2024-02-01 10:11:49 +0100

    Add files via upload

---
+++ crnogoricno_i_bjelogoricno.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadacic
+{
+    class Biljka
+    {
+    }
+
+
+
+    class Stablo : Biljka
+    {
+        bool otpadajuListovi;
+        
+        
+        public bool OtpadajuListovi    { get => otpadajuListovi; set => otpadajuListovi = value; }
+        public Stablo(bool otpadajuListovi)
+        {
+            
+            
+            OtpadajuListovi = otpadajuListovi;
+        }
+    }
+
+    class Cvijet : Biljka
+    {
+    }
+
+    class Crnogoricno : Stablo
+    {
+        public Crnogoricno() :base(true)
+        {
+            
+        }
+    }
+
+    class Bjelogoricno : Stablo
+    {
+        public Bjelogoricno() : base(false)
+        {
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bjelogoricno Hrast = new Bjelogoricno();
+            Crnogoricno Bor = new Crnogoricno();
+            
+            Console.Write($"Hrast: {Hrast.OtpadajuListovi}\nBor: {Bor.OtpadajuListovi}");
+
+            
+            Console.ReadKey();
+        }
+    }
+}
