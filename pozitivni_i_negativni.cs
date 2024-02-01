208d39f82402d39a166ebacddb201c58689a01e8
Luka Ivanić <107309593+LukaIvanic1@users.noreply.github.com>
2024-02-01 11:23:20 +0100

    Add files via upload

---
+++ pozitivni_i_negativni.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cijeli_i_decimalni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pozitivni x = new Pozitivni();
+            x.Broj = 100;
+            Console.WriteLine($"Apsolutna vrijednost od x: {x.ApsVrijednost()}");
+
+            Negativni y = new Negativni();
+            y.Broj = -100;
+            Console.WriteLine($"Apsolutna vrijednost od y: {y.ApsVrijednost()}");
+
+            Decimalni z = new Decimalni();
+            z.Broj = 100.45;
+            Console.WriteLine($"Apsolutna vrijednost od z: {z.ApsVrijednost()}");
+
+            Console.ReadKey();
+        }
+    }
+
+    class Brojevi
+    {
+        double broj;
+
+        public double Broj { get => broj; set => broj = value; }
+
+        public virtual double ApsVrijednost()
+        {
+            return 0;
+        }
+    }
+
+    class Cijeli : Brojevi
+    {
+        public override double ApsVrijednost()
+        {
+            return Math.Abs(Broj);
+        }
+    }
+    class Decimalni : Brojevi
+    {
+        public override double ApsVrijednost()
+        {
+            return Math.Abs(Broj);
+        }
+    }
+    class Pozitivni : Cijeli
+    {
+        public override double ApsVrijednost()
+        {
+            return Math.Abs(Broj);
+        }
+    }
+    class Negativni : Cijeli
+    {
+        public override double ApsVrijednost()
+        {
+            return Math.Abs(Broj);
+        }
+    }
+
+
+}
