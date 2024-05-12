af044e20b0b87aa0a09efa514b5045b0757bba9b
Luka Ivanić <107309593+LukaIvanic1@users.noreply.github.com>
2024-05-12 16:53:18 +0200

    Add files via upload

---
+++ Upis ljudi/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Upis_ljudi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.firstNameTextBox = new System.Windows.Forms.TextBox();
+            this.lastNameTextBox = new System.Windows.Forms.TextBox();
+            this.yearOfBirthTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // firstNameTextBox
+            // 
+            this.firstNameTextBox.Location = new System.Drawing.Point(149, 383);
+            this.firstNameTextBox.Name = "firstNameTextBox";
+            this.firstNameTextBox.Size = new System.Drawing.Size(251, 22);
+            this.firstNameTextBox.TabIndex = 0;
+            // 
+            // lastNameTextBox
+            // 
+            this.lastNameTextBox.Location = new System.Drawing.Point(149, 267);
+            this.lastNameTextBox.Name = "lastNameTextBox";
+            this.lastNameTextBox.Size = new System.Drawing.Size(251, 22);
+            this.lastNameTextBox.TabIndex = 1;
+            // 
+            // yearOfBirthTextBox
+            // 
+            this.yearOfBirthTextBox.Location = new System.Drawing.Point(149, 151);
+            this.yearOfBirthTextBox.Name = "yearOfBirthTextBox";
+            this.yearOfBirthTextBox.Size = new System.Drawing.Size(251, 22);
+            this.yearOfBirthTextBox.TabIndex = 2;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(149, 35);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(251, 22);
+            this.emailTextBox.TabIndex = 3;
+            this.emailTextBox.TextChanged += new System.EventHandler(this.emailTextBox_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(475, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 107);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Unesi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(475, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 107);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Spremi";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(101, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Prezime";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 267);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "E-mail";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(30, 389);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Godina rođenja";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.yearOfBirthTextBox);
+            this.Controls.Add(this.lastNameTextBox);
+            this.Controls.Add(this.firstNameTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Unos ljudi :D";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstNameTextBox;
+        private System.Windows.Forms.TextBox lastNameTextBox;
+        private System.Windows.Forms.TextBox yearOfBirthTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Upis ljudi/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Upis_ljudi
+{
+    public partial class Form1 : Form
+    {
+        private List<User> users = new List<User>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (string.IsNullOrWhiteSpace(firstNameTextBox.Text) ||
+                string.IsNullOrWhiteSpace(lastNameTextBox.Text) ||
+                string.IsNullOrWhiteSpace(emailTextBox.Text) ||
+                !emailTextBox.Text.Contains("@"))
+            {
+                MessageBox.Show("Molimo unesite ispravne podatke.");
+                return;
+            }
+
+            int yearOfBirth;
+            if (!int.TryParse(yearOfBirthTextBox.Text, out yearOfBirth) ||
+                yearOfBirth <= 0 ||
+                yearOfBirth > DateTime.Now.Year)
+            {
+                MessageBox.Show("Molimo unesite ispravnu godinu rođenja.");
+                return;
+            }
+
+            
+            User newUser = new User
+            {
+                FirstName = firstNameTextBox.Text,
+                LastName = lastNameTextBox.Text,
+                YearOfBirth = yearOfBirth,
+                Email = emailTextBox.Text
+            };
+
+           
+            users.Add(newUser);
+
+            
+            ClearFields();
+
+            MessageBox.Show("Korisnik uspješno dodan!");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (users.Count == 0)
+            {
+                MessageBox.Show("Nema podataka za spremanje.");
+                return;
+            }
+
+            
+            using (StreamWriter sw = new StreamWriter("users.csv"))
+            {
+                
+                sw.WriteLine("Ime,Prezime,Godina rođenja,E-mail");
+
+                
+                foreach (User user in users)
+                {
+                    sw.WriteLine($"{user.FirstName},{user.LastName},{user.YearOfBirth},{user.Email}");
+                }
+            }
+
+            MessageBox.Show("Podaci uspješno spremljeni u users.csv datoteku!");
+
+        }
+
+        private void ClearFields()
+        {
+            firstNameTextBox.Clear();
+            lastNameTextBox.Clear();
+            yearOfBirthTextBox.Clear();
+            emailTextBox.Clear();
+        }
+
+        private void emailTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int YearOfBirth { get; set; }
+        public string Email { get; set; }
+    }
+}
